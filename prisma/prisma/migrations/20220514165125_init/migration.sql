438c0b04dece8d443b8df63d22eeb8dd5fca4721
CesarGBkR <yexotortuesprin@gmail.com>
2022-05-14 11:54:00 -0500

    Adding gata fixed in database

---
+++ prisma/prisma/migrations/20220514165125_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `derrames` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
--- prisma/prisma/seed.js
+++ prisma/prisma/seed.js
@@ -4,41 +4,41 @@ const prisma = new PrismaClient();
 (async function main() {
     try {
         const derrame = await prisma.derrames.upsert({
-            where: { nombre: 'Guerra del Golfo' },
+            where: { nombre: "Guerra del Golfo" },
             update: {},
             create: {
-                nombre: 'Guerra del Golfo',
+                nombre: "Guerra del Golfo",
                 ubicacion: 'Golfo Pérsico',
-                paisesi: ["Irak", "Kuwait", "Arabia Saudita"],
-                causas:["causa 1", "causa2"],
-                consecuencias:["consecuencia 1","consecuencia 2"],
-                soluciones:["solucion 1", "solucion2"]
+                paisesi: "Irak, Kuwait, Arabia Saudita",
+                causas: "causa 1, causa2",
+                consecuencias: "consecuencia 1,consecuencia 2",
+                soluciones: "solucion 1, solucion2"
             },
         })
 
         const derrame2 = await prisma.derrames.upsert({
-            where: { nombre: 'Deepwater Horizon' },
+            where: { nombre: "Deepwater Horizon" },
             update: {},
             create: {
-                nombre: 'Deepwater Horizon',
+                nombre: "Deepwater Horizon",
                 ubicacion: 'Deepwater Horizon',
-                paisesi: ["Estados Unidos"],
-                causas:["causa 1"],
-                consecuencias:["consecuencia 1","consecuencia 2", "consecuencia 3"],
-                soluciones:["solucion 1"]
+                paisesi: "Estados Unidos",
+                causas:"causa 1",
+                consecuencias:"consecuencia 1, consecuencia 2, consecuencia 3",
+                soluciones:"solucion 1"
             },
         })
 
         const derrame3 = await prisma.derrames.upsert({
-            where: { nombre: 'Ixtoc' },
+            where: { nombre: "Ixtoc" },
             update: {},
             create: {
-                nombre: 'Ixtoc',
-                ubicacion: 'Golfo de México',
-                paisesi: ["México"],
-                causas:["causa 1", "causa 2", "causa 3"],
-                consecuencias:["consecuencia 1"],
-                soluciones:["solucion 1", "solucion2", "solucion 3"]
+                nombre: "Ixtoc",
+                ubicacion: "Golfo de México",
+                paisesi: "México",
+                causas:"causa 1, causa 2, causa 3",
+                consecuencias:"consecuencia 1",
+                soluciones:"solucion 1, solucion2, solucion 3"
             },
         })
 
