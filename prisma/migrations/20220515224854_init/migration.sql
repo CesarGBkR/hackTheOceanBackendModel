900f04ccfddb0ed236190d8127b167042a52378d
CesarGBkR <yexotortuesprin@gmail.com>
2022-05-15 17:55:39 -0500

    updating JSON, DB format and Endpoints

--- prisma/migrations/20220515224854_init/migration.sql
+++ prisma/migrations/20220515224854_init/migration.sql
@@ -1,7 +1,8 @@
 -- CreateTable
 CREATE TABLE `derrames` (
-    `id` INTEGER NOT NULL DEFAULT 0,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `nombre` VARCHAR(191) NOT NULL,
+    `date` VARCHAR(191) NOT NULL,
     `ubicacion` VARCHAR(191) NOT NULL,
     `paisesi` VARCHAR(191) NOT NULL,
     `causas` TEXT NOT NULL,
--- prisma/prisma/migrations/20220514165125_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `derrames` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
