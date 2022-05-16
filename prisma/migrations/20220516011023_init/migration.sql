f2024fe2aca5cbacdc5f7f2ee861279a16ed6ff2
CesarGBkR <yexotortuesprin@gmail.com>
2022-05-15 20:11:32 -0500

    Update DB local

---
+++ prisma/migrations/20220516011023_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `derrames` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(191) NOT NULL,
+    `fecha` VARCHAR(191) NOT NULL,
+    `ubicacion` VARCHAR(191) NOT NULL,
+    `paisesi` VARCHAR(191) NOT NULL,
+    `causas` TEXT NOT NULL,
+    `consecuencias` TEXT NOT NULL,
+    `soluciones` TEXT NOT NULL,
+
+    UNIQUE INDEX `derrames_nombre_key`(`nombre`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
